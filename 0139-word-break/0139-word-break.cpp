05af80bf9dc4a5e6b7eb98a45fa1194ba91d3141
study-with-bhawna-13 <study.with.bhawna.13@gmail.com>
2025-05-12 01:27:07 +0530

    Time: 3 ms (71.46%), Space: 10.5 MB (84.31%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size(), m = wordDict.size();
+        vector<bool> dp(n + 1, false);
+        dp[n] = true;
+        for (int i = n - 1; i >= 0; i--) {
+            for (auto w : wordDict) {
+                if (i + w.size() <= n && s.substr(i, w.size()) == w) {
+                    dp[i] = dp[i + w.size()] || dp[i];
+                }
+            }
+        }
+        return dp[0];
+    }
+};
