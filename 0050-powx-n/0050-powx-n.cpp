1e356bb4eaf411100701795bfdea3e8c4aecb5f1
study-with-bhawna-13 <study.with.bhawna.13@gmail.com>
2025-09-13 22:34:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.67%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,37 @@
+// class Solution {
+// public:
+//     double bPow(double x, long n){
+//         if(n==0 || x==1) return 1;
+//         if(n==1) return x;
+//         if(n<0) return bPow(1/x,-n);
+//         if(n%2 == 0) {
+//             double y=bPow(x,n/2);
+//             return y*y;
+//         }else{
+//             double y=bPow(x,(n-1)/2);
+//             return x*y*y;
+//         }
+//     }
+//     double myPow(double x, int n) {
+//         return bPow(x,n);
+//     }
+// };
 class Solution {
 public:
-    double bPow(double x, long n){
-        if(n==0 || x==1) return 1;
-        if(n==1) return x;
-        if(n<0) return bPow(1/x,-n);
-        if(n%2 == 0) {
-            double y=bPow(x,n/2);
-            return y*y;
-        }else{
-            double y=bPow(x,(n-1)/2);
-            return x*y*y;
-        }
-    }
     double myPow(double x, int n) {
-        return bPow(x,n);
+        long long exp = n;
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+        double result = 1.0;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+        return result;
     }
 };
